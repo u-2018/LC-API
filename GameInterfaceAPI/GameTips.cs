9ad4afbb0dbbfd8dd17b847930ddae39026e315c
2018 <149338847+u-2018@users.noreply.github.com>
2023-11-28 20:10:05 -0500

    Update GameTips.cs

--- GameInterfaceAPI/GameTips.cs
+++ GameInterfaceAPI/GameTips.cs
@@ -7,12 +7,18 @@ using UnityEngine;
 
 namespace LC_API.GameInterfaceAPI
 {
+    /// <summary>
+    /// Allows for displaying information through the tip box on the players screen, without any tips overlapping.
+    /// </summary>
     internal class GameTips
     {
         private static List<string> tipHeaders = new List<string>();
         private static List<string> tipBodys = new List<string>();
         private static float lastMessageTime;
 
+        /// <summary>
+        /// Add a tip to the tip que.
+        /// </summary>
         public static void ShowTip(string header, string body)
         {
             tipHeaders.Add(header);
