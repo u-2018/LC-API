4b71e4893e4979fd086c8abb63ee0cdf3c27826b
steven4547466 <steveowebster8@gmail.com>
2024-01-11 19:50:03 -0500

    Update GrabbingItemEventArgs.cs

--- LC-API/GameInterfaceAPI/Events/EventArgs/Player/GrabbingItemEventArgs.cs
+++ LC-API/GameInterfaceAPI/Events/EventArgs/Player/GrabbingItemEventArgs.cs
@@ -1,13 +1,30 @@
 ﻿namespace LC_API.GameInterfaceAPI.Events.EventArgs.Player
 {
+    /// <summary>
+    /// Contains all the information before a <see cref="Features.Player"/> picks up an <see cref="Features.Item"/>.
+    /// </summary>
     public class GrabbingItemEventArgs : System.EventArgs
     {
+        /// <summary>
+        /// The <see cref="Features.Player"/> picking up the <see cref="Features.Item"/>.
+        /// </summary>
         public Features.Player Player { get; }
 
+        /// <summary>
+        /// The <see cref="Features.Item"/> being picked up.
+        /// </summary>
         public Features.Item Item { get; }
 
+        /// <summary>
+        /// Gets or sets whether this is allowed to occur.
+        /// </summary>
         public bool IsAllowed { get; set; } = true;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GrabbingItemEventArgs"/> class.
+        /// </summary>
+        /// <param name="player"><inheritdoc cref="Player" /></param>
+        /// <param name="item"><inheritdoc cref="Item" /></param>
         public GrabbingItemEventArgs(Features.Player player, Features.Item item)
         {
             Player = player;
